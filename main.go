9c1afb5e79bd262a0321cfd9a6059f05c235a361
anupong.p <anupong.p@aware.co.th>
2024-12-04 08:00:22 +0700

    add comment

--- main.go
+++ main.go
@@ -11,13 +11,16 @@ import (
 )
 
 func main() {
+	// connect databse
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
+	// auto migrate
 	db.AutoMigrate(&todo.Todo{})
 
+	// Gin Routes
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -28,5 +31,6 @@ func main() {
 	handler := todo.NewTodoHandler(db)
 	r.POST("/todos", handler.NewTask)
 
+	// Run
 	r.Run()
 }
